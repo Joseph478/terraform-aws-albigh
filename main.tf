bccc9eeb7452535be86296b1153b83953d7be9f1
Joseph <josehparedes255@gmail.com>
2026-09-01 00:06:19 -0500

    fix(terraform): module

--- main.tf
+++ main.tf
@@ -151,6 +151,14 @@ resource "aws_security_group" "security_group_ec2" {
         security_groups = [aws_security_group.security_group_alb.id]
     }
 
+    ingress {
+        description     = "Trafic HTTPS from ALB"
+        from_port       = 8080
+        to_port         = 8080
+        protocol        = "tcp"
+        security_groups = [aws_security_group.security_group_alb.id]
+    }
+
     egress {
         from_port        = 0
         to_port          = 0
